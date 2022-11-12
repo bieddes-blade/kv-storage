a066d74883d062548d48fbb4d532fc11e15ffcbc
Yulia Rovnik <32664910+bieddes-blade@users.noreply.github.com>
2022-11-12 21:26:49 +0300

    Add files via upload

--- client/stub.hpp
+++ client/stub.hpp
@@ -1,1 +1,34 @@
+#pragma once
 
+#include <kv/types.hpp>
+
+#include <whirl/rpc/use/channel.hpp>
+
+#include <await/fibers/sync/future.hpp>
+#include <await/fibers/core/await.hpp>
+
+namespace kv {
+
+using await::fibers::Await;
+using whirl::rpc::TChannel;
+
+//////////////////////////////////////////////////////////////////////
+
+class KVBlockingStub {
+ public:
+  explicit KVBlockingStub(TChannel& channel) : channel_(channel) {
+  }
+
+  void Set(Key k, Value v) {
+    Await(channel_.Call("KV.Set", k, v).As<void>()).ExpectOk();
+  }
+
+  Value Get(Key k) {
+    return Await(channel_.Call("KV.Get", k).As<Value>()).Value();
+  }
+
+ private:
+  TChannel& channel_;
+};
+
+}  // namespace kv
